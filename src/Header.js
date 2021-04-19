f2f4fc31c4bcfbc17e5ad8b95fb2016cb214146c
Clément Dussollier <clem@Mac-mini-de-Clement.local>
2021-04-19 22:33:21 +0200

    Button Component & search header btn

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import './Header.css';
 import logo from './logo.svg';
 import SignWithGoogleButton from './shared/components/SignWithGoogleButton/SignWithGoogleButton';
+import Button from './shared/components/Button/Button';
 
 import {Link} from 'react-router-dom';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from '@material-ui/core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import firebase from 'firebase';
 
@@ -14,6 +17,18 @@ const provider = new firebase.auth.GoogleAuthProvider();
 const fromTopTransition = React.forwardRef(function fromTopTransition(props, ref){
 	return <Slide direction="down" ref={ref} {...props} />;
 });
+
+/**
+ * Agit au changement de la valeur du champ input de recherche
+ * @param {String} value Valeur du champ input
+ */
+function onChangeSearch(value){
+	if(value.length > 0){
+		document.getElementById('search-button').style.display = 'block';
+	}else{
+		document.getElementById('search-button').style.display = 'none';
+	}
+}
 
 function Header({user, firebaseApp}) {
 	const [loginOpen, setLoginOpen] = useState(false);
@@ -26,7 +41,8 @@ function Header({user, firebaseApp}) {
         </Link>
         <div className="right-elements">
 			<div className="search-bar">
-				<input placeholder="Rechercher" />
+				<input placeholder="Rechercher" onChange={(event)=>onChangeSearch(event.target.value)} />
+				<button id="search-button" className="search-button" title="Rechercher"><FontAwesomeIcon icon={faSearch} /></button>
 			</div>
 			{user &&
 				<div className="user-settings">
@@ -69,7 +85,7 @@ function Header({user, firebaseApp}) {
 				<SignWithGoogleButton firebaseApp={firebaseApp} setLoginOpen={setLoginOpen} />
 			</DialogContent>
 			<DialogActions>
-				<Button color={'secondary'} onClick={() => setLoginOpen(false)}>ANNULER</Button>
+				<Button theme={'error'} outlined={true} onClick={() => setLoginOpen(false)} text={'ANNULER'}></Button>
 			</DialogActions>
 		</Dialog>
     </div>
